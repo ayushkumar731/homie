fb7a40a68b3320042278b4a00e1d142ab572eecd
Ayush Kumar <kumarayush731@gmail.com>
2020-11-07 22:04:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// const dotenv = require('dotenv');
+// dotenv.config({ path: './config.env' });
+
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, (err) => {
+  if (err) {
+    console.log(`App is not running on port ${port}`);
+  }
+  console.log(`App is listening on port ${port}`);
+});

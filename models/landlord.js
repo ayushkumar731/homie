fd47c73f3df2f6ac8330c09e4f140efe33309a4f
Ayush Kumar <kumarayush731@gmail.com>
2020-11-07 22:49:05 +0530

    add landlord models

---
+++ models/landlord.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const LandlordSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "please provide email id"],
+    },
+    name: {
+      type: String,
+      required: [true, "please provide name"],
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      required: [true, "please provide password"],
+      select: false,
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "please provide confirm password"],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "password and confirm password are not same",
+      },
+    },
+    role: {
+      type: String,
+      default: landlord,
+      enum: ["landlord"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+LandlordSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
+});
+
+LandlordSchema.methods.checkPassword = async function (formPass, userPass) {
+  return await bcrypt.compare(formPass, userPass);
+};
+
+const landlord = mongoose.model("Landlord", LandlordSchema);
+module.exports = landlord;
